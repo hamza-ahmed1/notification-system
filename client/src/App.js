ec8b8eaad4a859ed3ad61582587adab5e50a8533
Hamza <khumza675@gmail.com>
2024-08-17 20:36:48 +0500

    Select section front-end added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import SelectSection from './components/SelectSection';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="">
       <h1>notification App</h1>
+      <p>Select the section</p>
+      <SelectSection/>
       </header>
     </div>
   );
---
+++ client/src/components/SelectSection.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function SelectVariants() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setAge(event.target.value);
+ 
+  };
+
+  return (
+    <div>
+      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-filled-label">Select Section</InputLabel>
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>Select Section from your section</em>
+          </MenuItem>
+          <MenuItem value={'BSCS-1A'}>BSCS-1A</MenuItem>
+          <MenuItem value={'BSCS-2C'}>BSCS-2C</MenuItem>
+          <MenuItem value={'BSSE-3B'}>BSSE-3B</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
